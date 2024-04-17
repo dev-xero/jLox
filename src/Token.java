39a4cb3c6a10490f098299a4c5e7b629fd349e4d
Xero kun <elo48858@gmail.com>
2024-04-17 17:22:47 +0100

    docs: Add comments to token class

--- src/Token.java
+++ src/Token.java
@@ -1,9 +1,14 @@
+/**
+ * Token class
+ * contains information on the token including its type, lexeme and literal
+ */
 public class Token {
     final TokenType type;
     final String lexeme;
     final Object literal;
     final int line;
 
+    // Initializes a new token
     Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
         this.lexeme = lexeme;
@@ -11,6 +16,7 @@ public class Token {
         this.line = line;
     }
 
+    // Returns information on the token as a string
     public String toString() {
         return this.type + " " + this.lexeme + " " + this.literal;
     }
