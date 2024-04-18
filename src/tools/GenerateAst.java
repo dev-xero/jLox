4e03d8607a3880a9f166fb13efb748e04c615ece
Xero kun <elo48858@gmail.com>
2024-04-18 21:45:19 +0100

    feat: Define method to build ast types

--- src/tools/GenerateAst.java
+++ src/tools/GenerateAst.java
@@ -1,6 +1,9 @@
 package tools;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 public class GenerateAst {
     public static void main(String[] args) throws IOException {
@@ -9,5 +12,52 @@ public class GenerateAst {
             System.exit(64);
         }
         String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outDir, String basename, List<String> types) throws IOException {
+        String path = outDir + "/" + basename + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class" + basename + "{");
+
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineTypes(writer, className, basename, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineTypes(PrintWriter writer, String className, String baseName, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + "{");
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        String[] fields = fieldList.split(",");
+
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+        writer.println();
+
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
     }
 }
